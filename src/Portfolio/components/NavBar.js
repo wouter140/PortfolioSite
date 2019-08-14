6bb74bb48da8da40669dd9ffa8746367dbbedf7b
Wouter <wouter.grutter@hotmail.com>
2019-08-15 01:51:01 +0200

    Adjusted scroll speed and nav image rounding and show transition

--- src/Portfolio/components/NavBar.js
+++ src/Portfolio/components/NavBar.js
@@ -31,9 +31,9 @@ export default class PortfolioNavBar extends Component {
             <MDBNavbar color="bg-primary" fixed="top" light expand="md" scrolling transparent>
                 <MDBContainer>
                     <MDBNavbarBrand className="d-flex clickable" onClick={ () => animateScroll.scrollToTop() }>
-                        <div className="mr-3">
+                        <div className="navbar-icon">
                             <img src={ ProfilePicture } alt="Wouter Grutter - Software Developer"
-                                className="rounded-circle" style={{height: "2.5rem"}}/>
+                                className="nav-profile-picture mr-3" style={{height: "2.5rem"}}/>
                         </div>
                         <strong className="align-self-center">Wouter Grutter</strong>
                     </MDBNavbarBrand>
@@ -41,22 +41,22 @@ export default class PortfolioNavBar extends Component {
                                       onClick={() => this.setState((state) => {return {collapse: !state.collapse}})}/>
                     <MDBCollapse isOpen={this.state.collapse} navbar>
                         <MDBNavbarNav left>
-                            <Link activeClass="active" to="about-page" spy={true} smooth={true} offset={-80} duration={500}>
+                            <Link activeClass="active" to="about-page" spy={true} smooth={true} offset={-80} duration={1000}>
                                 <MDBNavItem>
                                     <MDBNavLink>About</MDBNavLink>
                                 </MDBNavItem>
                             </Link>
-                            <Link activeClass="active" to="professional-projects-page" spy={true} smooth={true} offset={-40} duration={500}>
+                            <Link activeClass="active" to="professional-projects-page" spy={true} smooth={true} offset={-40} duration={1000}>
                                 <MDBNavItem>
                                     <MDBNavLink>Professional Projects</MDBNavLink>
                                 </MDBNavItem>
                             </Link>
-                            <Link activeClass="active" to="personal-projects-page" spy={true} smooth={true} offset={-40} duration={500}>
+                            <Link activeClass="active" to="personal-projects-page" spy={true} smooth={true} offset={-40} duration={1000}>
                                 <MDBNavItem>
                                     <MDBNavLink>Personal Projects</MDBNavLink>
                                 </MDBNavItem>
                             </Link>
-                            <Link activeClass="active" to="contact-page" spy={true} smooth={true} offset={-40} duration={500}>
+                            <Link activeClass="active" to="contact-page" spy={true} smooth={true} offset={-40} duration={1000}>
                                 <MDBNavItem>
                                     <MDBNavLink>Contact</MDBNavLink>
                                 </MDBNavItem>
