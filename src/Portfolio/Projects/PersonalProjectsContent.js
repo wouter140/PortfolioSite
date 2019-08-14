8f793b963295448faa828c985e2fb1f922567d8e
Wouter <wouter.grutter@hotmail.com>
2019-08-15 01:57:54 +0200

    Added and implemented projects expand and flexbox cards for nice mobile support

--- src/Portfolio/Projects/PersonalProjectsContent.js
+++ src/Portfolio/Projects/PersonalProjectsContent.js
@@ -3,46 +3,93 @@ import React from 'react';
 import ContentContainer from "../../components/ContentContainer";
 
 import "./ProjectsContent.scss";
-import {MDBCol, MDBContainer, MDBRow} from "mdbreact";
+import {MDBBtn, MDBCol, MDBCollapse, MDBContainer, MDBIcon, MDBRow} from "mdbreact";
 import ProjectCard from "./components/ProjectCard";
 
 export default class PersonalProjectsContent extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            expandProjects: false
+        }
+    }
+
     render() {
+        let containerClasses = "projects responsive-text-size";
+        if(this.state.expandProjects)
+            containerClasses += " expanded";
+
         return (
-            <ContentContainer id="personal-projects-page" className="projects responsive-text-size"
-                              contentTitle="Personal Projects" >
+            <ContentContainer id="personal-projects-page" className={ containerClasses }
+                              contentTitle={ (
+                                  <React.Fragment>
+                                      <span>Personal Projects</span>
+                                      <MDBBtn size="sm" className="rounded-circle float-right m-0 expand-projects-btn"
+                                            onClick={ () => this.setState((state) => { return {expandProjects: !state.expandProjects} }) }
+                                      >
+                                          <MDBIcon icon="angle-down" />
+                                      </MDBBtn>
+                                  </React.Fragment>
+                              ) } >
 
-                <MDBContainer>
-                    <MDBRow>
-                        <MDBCol md="6" xl="4">
-                            <ProjectCard imgAlt=""
-                                         title="Testing Title">
-                                Some super duper amazing card text.
+                <div className="d-flex flex-wrap justify-content-start">
+                    <MDBCol md="6" xl="4" className="animated fadeIn">
+                        <ProjectCard imgAlt=""
+                                     title="Testing Title">
+                            Some super duper amazing card text.
 
-                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
-                            </ProjectCard>
-                        </MDBCol>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
+                        </ProjectCard>
+                    </MDBCol>
 
-                        <MDBCol md="6" xl="4">
-                            <ProjectCard imgAlt=""
-                                         title="Testing Title">
-                                Some super duper amazing card text.
+                    <MDBCol md="6" xl="4" className="animated fadeIn">
+                        <ProjectCard imgAlt=""
+                                     title="Testing Title">
+                            Some super duper amazing card text.
 
-                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
-                            </ProjectCard>
-                        </MDBCol>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
+                        </ProjectCard>
+                    </MDBCol>
 
-                        <MDBCol md="8" xl="4" style={{margin: "0 auto"}}>
-                            <ProjectCard imgAlt=""
-                                         title="Testing Title">
-                                Some super duper amazing card text.
+                    <MDBCol md="6" xl="4" className="extra-project-md">
+                        <ProjectCard imgAlt=""
+                                     title="Testing Title">
+                            Some super duper amazing card text.
 
-                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
-                            </ProjectCard>
-                        </MDBCol>
-                    </MDBRow>
-                </MDBContainer>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
+                        </ProjectCard>
+                    </MDBCol>
 
+                    <MDBCol md="6" xl="4" className="extra-project">
+                        <ProjectCard imgAlt=""
+                                     title="Testing Title">
+                            Some super duper amazing card text.
+
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
+                        </ProjectCard>
+                    </MDBCol>
+
+                    <MDBCol md="6" xl="4" className="extra-project">
+                        <ProjectCard imgAlt=""
+                                     title="Testing Title">
+                            Some super duper amazing card text.
+
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
+                        </ProjectCard>
+                    </MDBCol>
+
+                    <MDBCol md="6" xl="4" className="extra-project">
+                        <ProjectCard imgAlt=""
+                                     title="Testing Title">
+                            Some super duper amazing card text.
+
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
+                        </ProjectCard>
+                    </MDBCol>
+
+                </div>
             </ContentContainer>
         );
     }
--- src/Portfolio/Projects/ProfessionalProjectsContent.js
+++ src/Portfolio/Projects/ProfessionalProjectsContent.js
@@ -4,16 +4,38 @@ import ContentContainer from "../../components/ContentContainer";
 import ProjectCard from "./components/ProjectCard";
 
 import "./ProjectsContent.scss";
-import {MDBCol, MDBContainer, MDBRow} from "mdbreact";
+import {MDBBtn, MDBCol, MDBCollapse, MDBContainer, MDBIcon, MDBRow} from "mdbreact";
 
 export default class ProfessionalProjectsContent extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            expandProjects: false
+        }
+    }
+
     render() {
+        let containerClasses = "projects responsive-text-size";
+        if(this.state.expandProjects)
+            containerClasses += " expanded";
+
         return (
-            <ContentContainer id="professional-projects-page" className="projects responsive-text-size"
-                              contentTitle="Professional Projects" >
-                <MDBContainer>
-                    <MDBRow>
-                        <MDBCol lg="6">
+            <ContentContainer id="professional-projects-page" className={ containerClasses }
+                              contentTitle={ (
+                                  <React.Fragment>
+                                      <span>Professional Projects</span>
+                                      <MDBBtn size="sm" className="rounded-circle float-right m-0 expand-projects-btn"
+                                              onClick={ () => this.setState((state) => { return {expandProjects: !state.expandProjects} }) }
+                                      >
+                                          <MDBIcon icon="angle-down" />
+                                      </MDBBtn>
+                                  </React.Fragment>
+                              ) } >
+                    <div className="d-flex flex-wrap justify-content-start">
+
+                        <MDBCol lg="6" className="animated fadeIn">
                             <ProjectCard imgAlt=""
                                          title="Testing Title">
                                 Some super duper amazing card text.
@@ -22,18 +44,7 @@ export default class ProfessionalProjectsContent extends React.Component {
                             </ProjectCard>
                         </MDBCol>
 
-                        <MDBCol lg="6">
-                            <ProjectCard imgAlt=""
-                                         title="Testing Title">
-                                Some super duper amazing card text.
-
-                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
-                            </ProjectCard>
-                        </MDBCol>
-                    </MDBRow>
-
-                    <MDBRow>
-                        <MDBCol md="6" xl="4">
+                        <MDBCol lg="6" className="animated fadeIn">
                             <ProjectCard imgAlt=""
                                          title="Testing Title">
                                 Some super duper amazing card text.
@@ -42,7 +53,7 @@ export default class ProfessionalProjectsContent extends React.Component {
                             </ProjectCard>
                         </MDBCol>
 
-                        <MDBCol md="6" xl="4">
+                        <MDBCol md="6" xl="4" className="animated fadeIn">
                             <ProjectCard imgAlt=""
                                          title="Testing Title">
                                 Some super duper amazing card text.
@@ -51,7 +62,7 @@ export default class ProfessionalProjectsContent extends React.Component {
                             </ProjectCard>
                         </MDBCol>
 
-                        <MDBCol md="8" xl="4" style={{margin: "0 auto"}}>
+                        <MDBCol md="6" xl="4" className="animated fadeIn">
                             <ProjectCard imgAlt=""
                                          title="Testing Title">
                                 Some super duper amazing card text.
@@ -59,8 +70,45 @@ export default class ProfessionalProjectsContent extends React.Component {
                                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
                             </ProjectCard>
                         </MDBCol>
-                    </MDBRow>
-                </MDBContainer>
+
+                        <MDBCol md="6" xl="4" className="extra-project-md">
+                            <ProjectCard imgAlt=""
+                                         title="Testing Title">
+                                Some super duper amazing card text.
+
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
+                            </ProjectCard>
+                        </MDBCol>
+
+                        <MDBCol md="6" xl="4" className="extra-project">
+                            <ProjectCard imgAlt=""
+                                         title="Testing Title">
+                                Some super duper amazing card text.
+
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
+                            </ProjectCard>
+                        </MDBCol>
+
+                        <MDBCol md="6" xl="4" className="extra-project">
+                            <ProjectCard imgAlt=""
+                                         title="Testing Title">
+                                Some super duper amazing card text.
+
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
+                            </ProjectCard>
+                        </MDBCol>
+
+                        <MDBCol md="6" xl="4" className="extra-project">
+                            <ProjectCard imgAlt=""
+                                         title="Testing Title">
+                                Some super duper amazing card text.
+
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
+                            </ProjectCard>
+                        </MDBCol>
+
+                    </div>
+
             </ContentContainer>
         );
     }
