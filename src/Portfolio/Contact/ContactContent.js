582f03cb8a44615c396342fd7b82724b151cb45f
Wouter <wouter.grutter@hotmail.com>
2019-08-14 04:41:48 +0200

    Added Contact form and added mail sending

--- src/Portfolio/Contact/ContactContent.js
+++ src/Portfolio/Contact/ContactContent.js
@@ -1,13 +1,104 @@
 import React from 'react';
 import ContentContainer from "../../components/ContentContainer";
+import {MDBBtn, MDBInput} from "mdbreact";
+
+import "./ContactContent.scss"
 
 export default class ContactContent extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            email: "",
+            phoneNr: "",
+            company: "",
+            message: ""
+        };
+
+        this.formRef = React.createRef();
+    }
+
+    static postData(url = '', data) {
+        // Default options are marked with *
+        return fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, cors, *same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrer: 'no-referrer', // no-referrer, *client
+            body: data, // body data type must match "Content-Type" header
+        })
+            .then(response => response.json()); // parses JSON response into native JavaScript objects
+    }
+
+    submitHandler = event => {
+        event.preventDefault();
+        event.target.className += " was-validated";
+
+        // Check if the form has all valid values so we can send the mail.
+        if(this.formRef.current.checkValidity()) {
+            ContactContent.postData('/API/Mail', this.state)
+                .then(data => {
+                    JSON.stringify(data); // JSON-string from `response.json()` call
+
+                    this.setState({
+                        name: "",
+                        email: "",
+                        phoneNr: "",
+                        company: "",
+                        message: ""
+                    });
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    };
+
     render() {
         return (
             <ContentContainer id="contact-page" className="responsive-text-size"
                               contentTitle="Contact" >
 
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere. Praesent turpis. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis
+                <form
+                    className="needs-validation"
+                    onSubmit={this.submitHandler}
+                    noValidate
+                    ref={ this.formRef }
+                >
+
+                    <MDBInput label="Name"
+                          required
+                          value={ this.state.name } onChange={(e) => this.setState({ name: e.target.value })} />
+
+                    <MDBInput type="email" label="Email"
+                          required
+                          value={ this.state.email } onChange={(e) => this.setState({ email: e.target.value })} />
+
+                    <MDBInput type="tel" label="Phone"
+                          value={ this.state.phoneNr } onChange={(e) => this.setState({ phoneNr: e.target.value })} />
+
+                    <MDBInput label="Company"
+                          value={ this.state.company } onChange={(e) => this.setState({ company: e.target.value })}/>
+
+                    <MDBInput type="textarea" label="Message" rows="6"
+                          required
+                          value={ this.state.message } onChange={(e) => this.setState({ message: e.target.value })}/>
+
+                    <div style={{height: "3.6rem"}}>
+                        <MDBBtn color="success" type="submit">
+                            Send Message
+                        </MDBBtn>
+                    </div>
+
+                </form>
+
             </ContentContainer>
         );
     }
