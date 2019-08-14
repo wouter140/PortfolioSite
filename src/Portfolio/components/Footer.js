8067dbced78684a969438e2c227252ac6901d6cc
Wouter <wouter.grutter@hotmail.com>
2019-08-15 01:50:06 +0200

    Added steam button in the footer

--- src/Portfolio/components/Footer.js
+++ src/Portfolio/components/Footer.js
@@ -25,6 +25,11 @@ export default class Footer extends React.Component {
                         <MDBIcon fab icon="stack-overflow" className="pr-1" /> Stack Overflow
                     </MDBBtn>
 
+                    <MDBBtn tag="a" social="steam" href="https://steamcommunity.com/id/wouter140/" target="_blank"
+                        title="Steam">
+                        <MDBIcon fab icon="steam" className="pr-1" /> Steam
+                    </MDBBtn>
+
                     <MDBBtn social="email" onClick={ () => {const x = window.open("mailto:info@woutergrutter.com"); x.close(); }}
                         title="Email Me">
                         <MDBIcon icon="envelope" className="pr-1" /> Email
@@ -47,6 +52,11 @@ export default class Footer extends React.Component {
                     <MDBBtn tag="a" size="lg" social="so" floating href="https://stackoverflow.com/users/5333948/wouter140" target="_blank"
                         title="Stack Overflow">
                         <MDBIcon fab icon="stack-overflow" />
+                    </MDBBtn>
+
+                    <MDBBtn tag="a" size="lg" social="steam" floating href="https://steamcommunity.com/id/wouter140/" target="_blank"
+                                     title="Steam">
+                        <MDBIcon fab icon="steam" />
                     </MDBBtn>
 
                     <MDBBtn size="lg" social="email" floating onClick={ () => {const x = window.open("mailto:info@woutergrutter.com"); x.close(); }}
