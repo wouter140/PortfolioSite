e589abe5d7dda81e247cbd9a2e0db60664c3804a
Wouter <wouter.grutter@hotmail.com>
2019-08-14 04:37:28 +0200

    Added footer

--- src/Portfolio/components/Footer.js
+++ src/Portfolio/components/Footer.js
@@ -1,11 +1,67 @@
 import React from 'react';
+import {MDBBtn, MDBContainer, MDBFooter, MDBIcon} from "mdbreact";
+
+import "./Footer.scss"
 
 export default class Footer extends React.Component {
     render() {
         return (
-            <footer>
-                Copyright © 2015 - { (new Date()).getFullYear() } Wouter Grutter. All Rights Reserved
-            </footer>
+            <MDBFooter className="font-small pt-4 mt-4">
+
+                <div className="text-center mb-4 social-buttons-text-large">
+
+                    <MDBBtn tag="a" social="li" href="https://www.linkedin.com/in/woutergrutter/" target="_blank"
+                        title="LinkedIn">
+                        <MDBIcon fab icon="linkedin-in" className="pr-1" /> LinkedIn
+                    </MDBBtn>
+
+                    <MDBBtn tag="a" social="git" href="https://github.com/wouter140" target="_blank"
+                        title="Github">
+                        <MDBIcon fab icon="github" className="pr-1" /> Github
+                    </MDBBtn>
+
+                    <MDBBtn tag="a" social="so" href="https://stackoverflow.com/users/5333948/wouter140" target="_blank"
+                        title="Stack Overflow">
+                        <MDBIcon fab icon="stack-overflow" className="pr-1" /> Stack Overflow
+                    </MDBBtn>
+
+                    <MDBBtn social="email" onClick={ () => {const x = window.open("mailto:info@woutergrutter.com"); x.close(); }}
+                        title="Email Me">
+                        <MDBIcon icon="envelope" className="pr-1" /> Email
+                    </MDBBtn>
+
+                </div>
+
+                <div className="text-center mb-4 social-buttons-small-round">
+
+                    <MDBBtn tag="a" size="lg" social="li" floating href="https://www.linkedin.com/in/woutergrutter/" target="_blank"
+                        title="LinkedIn">
+                        <MDBIcon fab icon="linkedin-in" />
+                    </MDBBtn>
+
+                    <MDBBtn tag="a" size="lg" social="git" floating href="https://github.com/wouter140" target="_blank"
+                        title="Github">
+                        <MDBIcon fab icon="github" />
+                    </MDBBtn>
+
+                    <MDBBtn tag="a" size="lg" social="so" floating href="https://stackoverflow.com/users/5333948/wouter140" target="_blank"
+                        title="Stack Overflow">
+                        <MDBIcon fab icon="stack-overflow" />
+                    </MDBBtn>
+
+                    <MDBBtn size="lg" social="email" floating onClick={ () => {const x = window.open("mailto:info@woutergrutter.com"); x.close(); }}
+                        title="Email Me">
+                        <MDBIcon icon="envelope" />
+                    </MDBBtn>
+
+                </div>
+
+                <div className="footer-copyright text-center py-3">
+                    <MDBContainer fluid>
+                        Copyright © 2015 - { (new Date()).getFullYear() } <a href="https://www.woutergrutter.com">Wouter Grutter</a>. All Rights Reserved
+                    </MDBContainer>
+                </div>
+            </MDBFooter>
         );
     }
 }
