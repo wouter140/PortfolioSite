3e2cd8f4421643ca51ad77b3ccb79eb2187c9a81
Wouter <wouter.grutter@hotmail.com>
2020-01-08 16:57:55 +0100

    Added PaintProject project page

--- src/ProjectPages/ProjectPages.js
+++ src/ProjectPages/ProjectPages.js
@@ -1,7 +1,10 @@
 import TestProject from "./ProjectPageComponents/TestProject";
 import BitBusterProject from "./ProjectPageComponents/BitBusterProject";
+import PaintProject from "./ProjectPageComponents/PaintProject";
 
 export const projects = [
     {path: "/TestProject", component: TestProject},
+
+    {path: "/Paint", component: PaintProject},
     {path: "/Bit-Buster", component: BitBusterProject},
 ];
