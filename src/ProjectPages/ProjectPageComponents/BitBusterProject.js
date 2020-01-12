cb1e3ef7c4dc90b5ee5f5f10c1f3431b0622b739
Wouter <wouter.grutter@hotmail.com>
2020-01-12 23:32:10 +0100

    Added further content to the bit-buster project page about my work

--- src/ProjectPages/ProjectPageComponents/BitBusterProject.js
+++ src/ProjectPages/ProjectPageComponents/BitBusterProject.js
@@ -28,13 +28,50 @@ export default function BitBusterProject(props) {
                   languages="C++"
                   targetPlatforms="PC (Itch.io)"
                   release={(
-                      <a href="https://igad.itch.io/bitbuster" target="_blank" rel="noopener noreferrer">Released on Itch.io</a>
+                      <a href="https://igad.itch.io/bitbuster" target="_blank" rel="noopener noreferrer" style={{color: "#007bff"}}>Released on Itch.io</a>
                   )}
             />
 
             <h4 className="text-white primary-color p-3">My Work</h4>
 
+            <h5 className="mt-4 font-weight-normal">UI Engine System</h5>
+            <p className="mb-2">
+                In Bit-Buster, I created the UI system inside the custom engine.
+                At the point that I joined the team, the custom engine was already quite far in development, but was missing any and all in-game UI systems.
+            </p>
+            <p>
+                For the UI system I started by creating the backbone that would handle all the input for the UI elements, focus and button presses.
+                This system is called the canvas and should be the parent object in the game for all the other UI elements.
+                I also implemented a base class that handles most UI element events and actions.
+                All the final UI objects eventually inherit from the base UI element to be controlled in-game through the engine.
+            </p>
 
+            <h6 className="font-weight-normal">UI Text</h6>
+            <p>
+                One of the UI elements I created was text. As text is quite important in any game to communicate with the user, this was the first element I created.
+
+            </p>
+
+            <h6 className="font-weight-normal">UI Button</h6>
+            <p>
+                I also created the Button UI element. Buttons are next to text very important to have in any game to be able to have user interaction with menu's.
+            </p>
+
+            <h6 className="font-weight-normal">UI Slider</h6>
+            <p>
+                Another UI element I created is a Slider. As we had planned to implement settings in our game, a slider would be very useful for settings like audio.
+                To optimize the implementation time of the slider, I also made it possible to be a progress bar. These are used in-game in various places like the player HUD.
+            </p>
+
+            <h5 className="mt-4 font-weight-normal">UI Development Tools</h5>
+            <p>
+                For the UI System I also implemented the in-engine tools for adding, controlling and editing these.
+            </p>
+
+            <h5 className="mt-4 font-weight-normal">UI Implementations</h5>
+            <p>
+                Next to creating the actual UI system in the engine, I also worked with it to implement various UI systems and elements in the game.
+            </p>
 
         </ProjectPage>
     )
