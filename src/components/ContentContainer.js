4fc02488c8117f78391e6c1b233a4a3845a1e76a
Wouter <wouter.grutter@hotmail.com>
2020-02-24 01:31:04 +0100

    Changed content container from div to section

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function ContentContainer(props) {
     const classes = "page-content-container w-100 " + props.className;
     return (
-        <div id={ props.id } className={ classes }>
+        <section id={ props.id } className={ classes }>
             <div className="inner-page-content">
                 <h3 className="inner-page-content-title pb-2">{ props.contentTitle }</h3>
                 { props.children }
             </div>
-        </div>
+        </section>
     );
 }
