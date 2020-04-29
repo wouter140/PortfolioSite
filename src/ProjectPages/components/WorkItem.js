78cb16a15a794b1ae4966ab65dbbec950273f010
Wouter <wouter.grutter@hotmail.com>
2020-04-30 00:04:00 +0200

    Implemented carousel into work items and properly vertically centred it

--- src/ProjectPages/components/WorkItem.js
+++ src/ProjectPages/components/WorkItem.js
@@ -1,25 +1,65 @@
 import * as React from "react";
+import * as PropTypes from "prop-types";
 
 import {Carousel} from "./Carousel";
+import {MDBCol} from "mdbreact";
 
 export function WorkItem(props) {
-    return <div className="row py-3 work-item-row">
-        <div className="col col-12">
-            <h5 className="mt-4 font-weight-normal">{props.title}</h5>
-            { props.children }
+    return (
+        <div className="row py-3 work-item-row">
+            <div className="col col-12">
+                {props.title.length > 0 && (
+                    <h5 className="mt-4 font-weight-normal">{props.title}</h5>
+                )}
+                { props.children }
+            </div>
         </div>
-    </div>;
+    );
+}
+WorkItem.propTypes = {
+    title: PropTypes.string
+};
+WorkItem.defaultProps = {
+    title: ""
 }
 
 export function WorkItemWithCarousel(props) {
     const carouselBefore = 'carouselBefore' in props && props.carouselBefore;
 
     return <div className="row py-3 work-item-row">
-        { carouselBefore && <Carousel/> }
-        <div className={"col col-6 col-lg-7 " + (carouselBefore ? "pl-2" : "pr-2")}>
-            <h5 className="mt-4 font-weight-normal">{props.title}</h5>
-            { props.children }
+        { carouselBefore && (
+            <MDBCol size="6" lg="5">
+                <Carousel className="work-item-carousel"
+                    contentSources={props.sources}
+                />
+            </MDBCol>
+        ) }
+
+        <div className={"col col-6 col-lg-7 flex-vertical-center " + (carouselBefore ? "pl-2" : "pr-2")}>
+            <div>
+                {props.title.length > 0 && (
+                    <h5 className="mt-4 font-weight-normal">{props.title}</h5>
+                )}
+                { props.children }
+            </div>
         </div>
-        { !carouselBefore && <Carousel/> }
+
+        { !carouselBefore && (
+            <MDBCol size="6" lg="5">
+                <Carousel className="work-item-carousel"
+                          contentSources={props.sources}
+                />
+            </MDBCol>
+        ) }
     </div>;
 }
+WorkItemWithCarousel.propTypes = {
+    sources: PropTypes.arrayOf(PropTypes.string),
+    carouselBefore: PropTypes.bool,
+    title: PropTypes.string
+};
+WorkItemWithCarousel.defaultProps = {
+    sources: [],
+    carouselBefore: false,
+    title: ""
+};
