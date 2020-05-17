c54e613b5a661a27b0460470cd482d725dd1ead3
Wouter <wouter.grutter@hotmail.com>
2020-05-17 22:58:16 +0200

    Updated discord presence plugin content

--- src/ProjectPages/ProjectPageComponents/DiscordPresencePlugin.js
+++ src/ProjectPages/ProjectPageComponents/DiscordPresencePlugin.js
@@ -14,16 +14,18 @@ export default function DiscordPresencePlugin() {
             title="Discord Rich Presence Plugin"
             imageURL={image}
         >
-            I developed an Unreal Engine 4 plugin that helps developers connect with Discord to easily update the
-            players' presence from the game.<br/>
-            Internally I used the Discord GameSDK. This also allows the plugin to implement the request to join, join
-            and spectate events.
+            I am currently developing an Unreal Engine 4 plugin that helps developers connect with Discord to easily
+            update the players' presence from within the game.<br/>
+            Internally I use the Discord GameSDK. This also allows the plugin to implement the request to join, join
+            and spectate events that get called from the Discord client.
 
             <ProjectStartLayout>
                 <ContributionHighlights>
                     <li>Integrated Discord GameSDK API.</li>
                     <li>Added Project Settings.</li>
-                    <li>Implemented Scripts to update Discord presence and handle API limitations.</li>
+                    <li>Implemented Discord Rich Presence</li>
+                    <li>Implemented Discord Game Session Functionality</li>
+                    <li>Implemented Discord Friends</li>
                     <li>Implemented blueprint that can be used in the level to update presence anywhere.</li>
                 </ContributionHighlights>
 
@@ -41,18 +43,21 @@ export default function DiscordPresencePlugin() {
             </ProjectStartLayout>
 
             <h4 className="text-white primary-color mt-5 p-3">My Work</h4>
-            <WorkItemWithCarousel
-                title="Discord GameSDK API"
-                sources={[]}
-                // sources={[
-                //     uiOptionsExampleVideo,
-                //     mainMenuVideo,
-                // ]}
-            >
-                <p>
-                    In progress
-                </p>
-            </WorkItemWithCarousel>
+            <p>
+                This project is currently in-progress. I will share more details when they become available.
+            </p>
+            {/*<WorkItemWithCarousel*/}
+            {/*    title="Discord GameSDK API"*/}
+            {/*    sources={[]}*/}
+            {/*    // sources={[*/}
+            {/*    //     uiOptionsExampleVideo,*/}
+            {/*    //     mainMenuVideo,*/}
+            {/*    // ]}*/}
+            {/*>*/}
+            {/*    <p>*/}
+            {/*        This project is currently in-progress. I will share more details when they become available. */}
+            {/*    </p>*/}
+            {/*</WorkItemWithCarousel>*/}
 
         </ProjectPage>
     )
